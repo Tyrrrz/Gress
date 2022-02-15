b400b73d8dfda3fd93b803518b7b91e59ee51213
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-02-15 19:35:52 +0200

    Make `DelegateCompletableProgress` and `DelegateProgress` public

--- Gress/Completable/DelegateCompletableProgress.cs
+++ Gress/Completable/DelegateCompletableProgress.cs
@@ -2,18 +2,27 @@
 
 namespace Gress.Completable;
 
-internal class DelegateCompletableProgress<T> : ICompletableProgress<T>
+/// <summary>
+/// Simple implementation of <see cref="ICompletableProgress{T}"/> that uses the provided
+/// delegates to report progress updates and signal completion.
+/// </summary>
+public class DelegateCompletableProgress<T> : ICompletableProgress<T>
 {
     private readonly Action<T> _report;
     private readonly Action _reportCompletion;
 
+    /// <summary>
+    /// Initializes an instance of <see cref="DelegateCompletableProgress{T}"/>.
+    /// </summary>
     public DelegateCompletableProgress(Action<T> report, Action reportCompletion)
     {
         _report = report;
         _reportCompletion = reportCompletion;
     }
 
+    /// <inheritdoc />
     public void Report(T value) => _report(value);
 
+    /// <inheritdoc />
     public void ReportCompletion() => _reportCompletion();
 }
--- Gress/DelegateProgress.cs
+++ Gress/DelegateProgress.cs
@@ -2,12 +2,23 @@
 
 namespace Gress;
 
-// Simple progress handler without thread scheduling
-internal class DelegateProgress<T> : IProgress<T>
+/// <summary>
+/// Simple implementation of <see cref="IProgress{T}"/> that uses the provided
+/// delegate to report progress updates.
+/// </summary>
+/// <remarks>
+/// Unlike <see cref="Progress{T}"/>, this implementation does not post updates
+/// through a synchronization context, but rather calls the delegate directly.
+/// </remarks>
+public class DelegateProgress<T> : IProgress<T>
 {
     private readonly Action<T> _report;
 
+    /// <summary>
+    /// Initializes an instance of <see cref="DelegateProgress{T}"/>.
+    /// </summary>
     public DelegateProgress(Action<T> report) => _report = report;
 
+    /// <inheritdoc />
     public void Report(T value) => _report(value);
 }
