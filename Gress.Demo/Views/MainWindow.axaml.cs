b06e862db4efc33ba792e41b5f87f1644a9e11c2
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-03-19 23:28:49 +0200

    Use expression-bodied members

--- Gress.Demo/Views/MainWindow.axaml.cs
+++ Gress.Demo/Views/MainWindow.axaml.cs
@@ -4,8 +4,5 @@ namespace Gress.Demo.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+    public MainWindow() => InitializeComponent();
 }
